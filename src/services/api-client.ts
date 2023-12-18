8eb4397848700fd8776d71e4ded5815389ec2f9d
Glenn Seymon <glenn@superdudes.com.au>
2023-12-18 15:47:48 +1100

    Refactoring - create a reusable API client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,43 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "b5616a484e594c668e0afeb6a3205c73",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string){
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then(res => res.data);
+/*    
+    (gameQuery: GameQuery) => useQuery<FetchResponse<T>,Error>({
+      queryKey: ["games", gameQuery],
+      queryFn: () => apiClient.get<FetchResponse<T>>("/games", {
+        params: {
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText
+        },
+      },).then(res => res.data),
+      staleTime: 24 * 60 * 60 * 1000, // 24 hours.
+    });
+*/
+  }
+}
+
+export default APIClient;
