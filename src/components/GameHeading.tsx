089868320ae1df2df372ff62fa60404cc851bee7
Glenn Seymon <glenn@superdudes.com.au>
2024-01-10 16:05:26 +1100

    Fix error.

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -13,7 +13,7 @@ const GameHeading = ({ gameQuery }: Props) => {
   const genre = genres?.results.find(g => g.id === gameQuery.genreId);
   const platform = platforms?.results.find(p => p.id === gameQuery.platformId);
 
-  const heading = `${gameQuery.platform?.name || ''} ${genre?.name || ''} Games`;
+  const heading = `${platform?.name || ''} ${genre?.name || ''} Games`;
 
   return (
     <Heading as='h1' marginY={5} fontSize='5xl'>{heading}</Heading>
